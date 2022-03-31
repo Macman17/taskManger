14770b3e0e03c76956d697450b33db26851599eb
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-03-31 10:33:52 -0700

    Added Updates
    Added a table to display task and save task functions

--- script/main.js
+++ script/main.js
@@ -2,6 +2,36 @@ var importanceIcon =  "fas fa-star";
 var nonImportanceIcon =  "far fa-star";
 var isImportance = true;
 var isPanelVisible = true;
+let userList= [];
+class Task{
+    constructor (important,title,description, color,startDate,dueDate,category){ 
+        this.important = important;
+        this.title = title;
+        this.description = description;
+        this.color= color;
+        this.startDate= startDate;
+        this.dueDate = dueDate;
+        this.category = category;
+    }
+}
+function isValid(task) {
+    //validate the user *** Extra Home work
+        
+   let valid=true;
+   $('input').removeClass("input-error");
+   if (task.title.length == 0) {
+       console.log("Please add a title");
+       valid=false;
+       $('#txtTitle').addClass("input-error");
+   }
+   if(!valid){
+       displayError("Missing data");
+  
+   }
+
+   return valid;
+}
+
 
 function toggleImportance() {
     console.log("Icon Clicked");
@@ -29,13 +59,72 @@ function hideForm() {
         isPanelVisible = true;
     }
 }
+function register(){
+    let inputImportant= $('#iImportant').val();
+    let inputTitle= $('#txtTitle').val();
+    let inputDescription= $('#txtDescription').val();
+    let inputColor= $('#txtColor').val();
+    let inputStartDate= $("#txtStartDate").val();
+    let inputDueDate= $('#txtDueDate').val();
+    let inputCategory= $("#txtCategory").val();
+
+
+    console.log(inputImportant,inputTitle,inputDescription,inputColor,inputStartDate,inputDueDate,inputCategory);
+    //create theUser
+    let theTask = new Task(inputImportant,inputTitle,inputDescription,inputColor,inputStartDate,inputDueDate,inputCategory);
+if (isValid(theTask)) {
+        saveTask(theTask);
+        //clear the inputs
+        $("input").val("");
+    }
+    
+}
+function displayTask(task){
+    //travel the array
+    $("#taskTable tbody").html("");
+    let data="";
+   for (let i = 0; i < task.length; i++) {
+        console.log(task[i].title);
+        data += createTask(task[i], i);
+        //data = data + createUser(users[i]);
+    }
+    $("#taskTable tbody").html(data);
+    //get each user
+    //display the user
+    
+    //append the user to the table
+    
+}
+function createTask(task, index) {
+    return `
+    <tr>
+    <td> ${task.important}</td>
+    <td> ${task.title}</td>
+    <td> ${task.description}</td>
+    <td> ${task.color}</td>
+    <td>${task.startDate}</td>
+    <td> ${task.dueDate}</td>
+    <td> ${task.category}</td>
+    <td><input type='button' value='Remove' onclick="removeUser(${index})" /></td>
+    </tr>
+    `;
+}
+function removeTask(index) {
+    remove(index);
+    let task=readUsers();
+    displayUsers(task);
+
+}
 
 
 
 function init() {
     console.log("Task Manager");
+    let task=readTask();
+    displayTask(task);
 
     //load data
+    
     
     //hook events
     $("#iImportant").click(toggleImportance);
---
+++ script/store.js
@@ -0,0 +1,29 @@
+const LS_KEY="task";
+function saveTask(task) {
+    //load  the old data
+    let data=readTask();
+    //marge old and new data
+    data.push(task);
+    //save
+    let val= JSON.stringify(data);//parse into a JSON String
+    localStorage.setItem(LS_KEY,val);//send the string to the local storage
+}
+function readTask() {
+    let data= localStorage.getItem(LS_KEY);//get data from LS
+    console.log(data);// JSON
+    if(!data){//is not data?
+        return [];//create the array
+    }else{
+        //we have data
+        let list=JSON.parse(data);//obj
+        return list;
+    }
+}
+function remove(index) {
+    let data=readTask();
+    //we remove the user in the given index from the array data
+    data.splice(index, 1);//must have the specific index and how many to delete
+    //save
+    let val= JSON.stringify(data);//parse into a JSON String
+    localStorage.setItem(LS_KEY,val);
+}
